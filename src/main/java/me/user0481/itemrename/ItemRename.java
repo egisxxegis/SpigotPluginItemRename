2228a72f2ec1ca08b24bd1bce11601190115d995
user0481 <egisxxegis@gmail.com>
2021-08-24 18:40:13 +0300

    Initial Minecraft Plugin

---
+++ src/main/java/me/user0481/itemrename/ItemRename.java
@@ -0,0 +1,12 @@
+package me.user0481.itemrename;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemRename extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Item rename has started. Remember to bring your token.");
+    }
+}
