63489eb3c2bdc22dd081f54510e8c03373893cf4
user0481 <egisxxegis@gmail.com>
2021-08-24 21:47:07 +0300

    Dummy command fix (after refactor)

--- src/main/java/me/user0481/itemrename/ItemRename.java
+++ src/main/java/me/user0481/itemrename/ItemRename.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import me.user0481.itemrename.commands.Pervadinti;
 
 public final class ItemRename extends JavaPlugin {
 
@@ -12,6 +13,8 @@ public final class ItemRename extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Item rename has started. Remember to bring your token.");
+        getCommand("pervadinti").setExecutor(new Pervadinti());
+
     }
 
 
