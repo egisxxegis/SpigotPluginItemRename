daac23da8a7c41e7a2a67ac1968f65e0bc287f3e
user0481 <egisxxegis@gmail.com>
2021-08-26 00:07:48 +0300

    Name GUI choices

--- src/main/java/me/user0481/itemrename/commands/Pervadinti.java
+++ src/main/java/me/user0481/itemrename/commands/Pervadinti.java
@@ -2,6 +2,7 @@ package me.user0481.itemrename.commands;
 
 import me.user0481.itemrename.Formatter;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -13,7 +14,9 @@ import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
@@ -39,9 +42,29 @@ public class Pervadinti implements CommandExecutor {
         ItemStack dialogYes         = new ItemStack(Material.GREEN_WOOL,1);
         ItemStack dialogNo          = new ItemStack(Material.RED_WOOL,1);
 
-        for(int i=0; i<=3; dialogInventory.setItem(i++,dialogYes));
-        dialogInventory.setItem(4,player.getInventory().getItemInMainHand());
-        for(int i=5; i<=8; dialogInventory.setItem(i++,dialogNo));
+        ItemMeta yesMeta = dialogYes.getItemMeta();
+        yesMeta.setDisplayName(ChatColor.DARK_GREEN + "Pirkti");
+        ArrayList<String> yesLore = new ArrayList<>();
+        yesLore.add(ChatColor.GOLD + "Pirkti daikto");
+        yesLore.add(ChatColor.GOLD + "pervadinimą.");
+        yesLore.add(ChatColor.GOLD + "Kaina: " + ChatColor.RED + "1 Keitimo lapelis" + ChatColor.GOLD + ".");
+        yesMeta.setLore(yesLore);
+        dialogYes.setItemMeta(yesMeta);
+
+        ItemMeta noMeta = dialogNo.getItemMeta();
+        noMeta.setDisplayName(ChatColor.RED + "Netinka");
+        ArrayList<String> noLore = new ArrayList<>();
+        noLore.add(ChatColor.GOLD + "Atšaukti daikto");
+        noLore.add(ChatColor.GOLD + "pervadinimą.");
+        noLore.add(ChatColor.GOLD + "Keitimo lapelis nebus");
+        noLore.add(ChatColor.GOLD + "panaudotas");
+        noMeta.setLore(noLore);
+        dialogNo.setItemMeta(noMeta);
+
+        int mainItemIndex = 4;
+        for(int i=0; i<=mainItemIndex-1; dialogInventory.setItem(i++,dialogYes));
+        dialogInventory.setItem(mainItemIndex,player.getInventory().getItemInMainHand());
+        for(int i=mainItemIndex+1; i<=8; dialogInventory.setItem(i++,dialogNo));
 
 
         player.openInventory(dialogInventory);
