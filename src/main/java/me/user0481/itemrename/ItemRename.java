ca9b56b3d0baacc961f1e88aac9d92353e0ea40b
user0481 <egisxxegis@gmail.com>
2021-08-27 00:04:22 +0300

    Register inventory close event

--- src/main/java/me/user0481/itemrename/ItemRename.java
+++ src/main/java/me/user0481/itemrename/ItemRename.java
@@ -1,6 +1,7 @@
 package me.user0481.itemrename;
 
 import me.user0481.itemrename.events.InventoryClickEvent;
+import me.user0481.itemrename.events.InventoryCloseEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.user0481.itemrename.commands.PervadintiCommand;
 
@@ -12,7 +13,7 @@ public final class ItemRename extends JavaPlugin {
         System.out.println("Item rename has started. Remember to bring your token.");
         getCommand("pervadinti").setExecutor(new PervadintiCommand());
         getServer().getPluginManager().registerEvents(new InventoryClickEvent(),this);
-
+        getServer().getPluginManager().registerEvents(new InventoryCloseEvent(),this);
     }
 
 
---
+++ src/main/java/me/user0481/itemrename/events/InventoryCloseEvent.java
@@ -0,0 +1,26 @@
+package me.user0481.itemrename.events;
+
+import me.user0481.itemrename.Formatter;
+import me.user0481.itemrename.config.Config;
+import me.user0481.itemrename.config.ConfigFactory;
+import me.user0481.itemrename.handler.PervadintiHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+
+public class InventoryCloseEvent implements Listener {
+
+    @EventHandler
+    public void InventoryCloseEvent(org.bukkit.event.inventory.InventoryCloseEvent e) {
+        Config config = ConfigFactory.getConfig();
+        if(!e.getView().getTitle().equalsIgnoreCase(config.getGUITitle()))
+            return;
+
+        Player player = (Player) e.getPlayer();
+        PervadintiHandler handler = new PervadintiHandler(player);
+        handler.releaseHandler();
+        player.sendMessage(Formatter.formatMessage("Pervadinimas atšauktas."));
+
+    }
+}
